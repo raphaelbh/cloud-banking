325a5fcc2f9efdcadf0f4a4cfd348bf50d2c22a9
Raphael Oliveira <raphaeldias.ti@gmail.com>
2022-01-12 10:39:55 -0300

    update lambda get transactions by user

--- lambda-get-transactions-by-user/local.py
+++ lambda-get-transactions-by-user/local.py
@@ -1,4 +1,17 @@
+import os
 from  index import lambda_handler
 
+###  ENVIRONMENT VARIABLES  ###
+os.environ["LOCALSTACK_HOSTNAME"] = 'localhost'
+
+event = {
+    'queryStringParameters': {
+        'user': 'john'
+    }
+}
+
+context = {}
+
 ###  CALLING LAMBDA FUNCTION  ###
-lambda_handler({}, {})
+response = lambda_handler(event, context)
+print(response)
